334e489d4ca3c5f452bc8819f8bb83e7da1fe003
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2015-07-10 01:47:44 -0500

    Angular validation and submit handler

--- source/javascript/main.js
+++ source/javascript/main.js
@@ -37,65 +37,40 @@
             contact.lastName    = '';
             contact.email       = '';
             contact.city        = '';
-            contact.valid       = false;
-            var _length;
+            contact.received    = false;
+            contact.form        = document.getElementsByTagName( 'form' );
+            contact.url         = contact.form[0].action + '?action=contact';
+            contact.response    = '';
 
-            contact.validateFirstName = function ( ) {
-                _length = contact.firstName.length;
-                contact.valid = !contact._validateMinLength( _length, 2 );
-                contact.valid = !contact._validateMaxLength( _length, 140 );
-                return contact.valid;
-            };
+            contact.submit      = function ( e ) {
+                e.preventDefault();
+                e.stopPropagation();
 
-            contact.validateLastName = function ( ) {
-                _length = contact.lastName.length;
-                contact.valid = !contact._validateMinLength( _length, 2 );
-                contact.valid = !contact._validateMaxLength( _length, 140 );
-                contact.valid = !contact._validateMail( contact.lastName );
-                return contact.valid;
-            };
-
-            contact.validateEmail = function ( ) {
-                _length = contact.email.length;
-                contact.valid = !contact._validateMinLength( _length, 2 );
-                contact.valid = !contact._validateMaxLength( _length, 140 );
-                return contact.valid;
-            };
-
-            contact.validateCity = function ( ) {
-                _length = contact.city.length;
-                contact.valid = !contact._validateMinLength( _length, 2 );
-                contact.valid = !contact._validateMaxLength( _length, 140 );
-                return contact.valid;
-            };
-
-            contact.validateMessage = function () {
-                _length = contact.message.length;
-                contact.valid = !contact._validateMinLength( _length, 2 );
-                contact.valid = !contact._validateMaxLength( _length, 140 );
-                return contact.valid;
-            }
-
-            contact._validateMail = function(mail) {
-                return(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i.test(mail)) ? true : false;
-            };
-            contact._validateNumber = function(numberToCheck) {
-                return /^\d+[^a-zA-Z]+$/.test(parseInt(numberToCheck));
-            };
-            contact._validateRange = function(rangeTo, rangeFrom, valueToCheck) {
-                return(rangeTo >= valueToCheck && rangeFrom <= valueToCheck) ? true : false;
-            };
-            contact._validateMinLength = function(minLength, valueToCheck) {
-                return(minLength < valueToCheck) ? true : false;
-            };
-            contact._validateMaxLength = function(maxLength, valueToCheck) {
-                return(valueToCheck <= maxLength) ? true : false;
-            };
-            contact._validateDate = function(dateToCheck) {
-                return(!/Invalid|NaN/.test(new Date(dateToCheck).toUTCString())) ? true : false;
+                $.ajax( contact.url, {
+                    converters: {
+                            "* text": window.String,
+                            "text html": true,
+                            "text json": jQuery.parseJSON,
+                            "text xml": jQuery.parseXML
+                    },
+                    data: {
+                        firstName:  contact.firstName,
+                        lastName:   contact.lastName,
+                        email:      contact.email,
+                        city:       contact.city
+                    },
+                    type: 'POST',
+                    dataType: 'json'
+                } ).done( function ( responseText ) {
+                    contact.response = responseText.message;
+                    contact.received = true;
+                } ).fail( function ( ) {
+                    contact.response = 'Hubo un error. Por favor, intenta de nuevo.';
+                    contact.received = true;
+                } );
             };
         } );
-    } )()
+    } )();
 
     //  When page is finished loaded
     $( document ).ready( function () {
@@ -118,7 +93,9 @@
         if ( $( '.gallery' ).exists() ) {
             var _height = $( window ).innerHeight() + 'px';
             $( 'window' ).on( 'resize', function ( e ) {
-                _height = $( window ).innerHeight() + 'px';position()
+                e.preventDefault();
+                e.stopPropagation();
+                _height = $( window ).innerHeight() + 'px';
                 $( '.gallery' ).height( _height );
             } );
         }
